a4031ea2229af7be9a70f1371431d0dab42b30a7
Bharat Rajan <Bharat_Rajan@comcast.com>
2016-05-11 13:47:46 -0700

    Passport JS and Google auth

---
+++ config/auth.js
@@ -0,0 +1,11 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+    'googleAuth' : {
+        'clientID'      : '647946652410-rvr8u629n6lesn0fgkk3a2dm5e67evit.apps.googleusercontent.com',
+        'clientSecret'  : 'xUgrIJG-WMx_EjTpIxUGfzy_',
+        'callbackURL'   : 'http://localhost:8081/table.html'
+    }
+
+};
